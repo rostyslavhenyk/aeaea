56daa8596d14e0bf94c0d6145658be1507ecc92c
rostyslavhenyk <vlcz9299@leeds.ac.uk>
2025-02-06 20:01:36 +0000

    a

--- game/main (1).py
+++ game/main (1).py
@@ -10,8 +10,7 @@ init_sensor()
 init_gas_sensor()
 init_display()
 players = ["A1","A0","B0","B1","C0","C1","D1","D0"]
-userID = "A"
-
+userID = "C"
 while True:
     incoming = radio.receive()
     if incoming == "are you ready":
@@ -21,8 +20,8 @@ while True:
             pass
         show("",2)
         show("",3)
-        radio.send("userA")
-        display.show("A", wait=False)
+        radio.send("userC")
+        display.show("C", wait=False)
     if incoming == "send the game":
         round = 1
         while round <= 10:
@@ -32,14 +31,16 @@ while True:
             if roundInfo and roundInfo.startswith("type"):
                 if len(roundInfo[4:]) == 2:
                     gameForRound = int(roundInfo[4])
-                    simonSays = int(roundInfo[5])
+                    simonSays = roundInfo[5]
                 elif len(roundInfo[4:]) == 3:
                     gameForRound = int(roundInfo[4]+roundInfo[5])
-                    simonSays = int(roundInfo[6])
+                    simonSays = roundInfo[6]
                 game(round, games[gameForRound], simonSays)
                 round += 1
             elif roundInfo and roundInfo in players:
                 pass
+
+
     if incoming == "win"+userID:
         show("",0)
         show("",1)
@@ -50,11 +51,7 @@ while True:
         show("",1)
         show("Loser!",2)
         break
+        
 
 
-# show on third line waiting for other players (?)
-#try to send the send the game signal multiple times
-# "simon says" at the start of round task
-#better formulate the round tasks
-# have temp co2 and light level(?) on the screen at all times(?)
     
--- game/main_game (1).py
+++ game/main_game (1).py
@@ -5,7 +5,7 @@ import music
 import radio
 import random
 
-userID = "A"
+userID = "C"
 init_display()
 init_sensor()
 init_gas_sensor()
@@ -32,16 +32,12 @@ def check_action(action):
                 for i in range(len(actions)):
                     if i != buttons_index and actions[i]:
                         return "fail"
-    """ if random.randint(0,100) < 80:
-        simonSays = "Simon says "
-    else:
-        simonSays = """
+
 def game(round, action, simon):
     show("Round: " + str(round), 0)
-    sleep(200)
     if simon == "1":  
         show("Simon Says "+action, 1)
-    else:
+    elif simon == "0":
         show(action, 1)
 
     running = True
@@ -64,11 +60,9 @@ def game(round, action, simon):
                 result = check_action(action)
                 
                 if result == "pass":
-                    display.show(Image.YES)
                     success = "1"
                     running = False
                 elif result == "fail":
-                    display.show(Image.NO)
                     success = "0"
                     running = False
                     
@@ -78,7 +72,6 @@ def game(round, action, simon):
                 show("{} C".format(tempCount), 6)
                 sleep(50)
                 if tempCount> calcedTemp+1:
-                    display.show(Image.YES)
                     success = "1"
                     running = False
             elif action.lower() == "blow on me":
@@ -87,7 +80,6 @@ def game(round, action, simon):
                 show("{}".format(currentCO2)+"ppm", 6)
                 sleep(50)
                 if CO2> currentCO2-15:
-                    display.show(Image.YES)
                     success = "1"
                     running = False
             elif action.lower() == "shout":
@@ -96,83 +88,68 @@ def game(round, action, simon):
                 show(str(microphone.sound_level()),6)
                 sleep(50)
                 if microphone.current_event() == SoundEvent.LOUD:
-                    display.show(Image.YES)
                     success = "1"
                     running = False
             elif action.lower() == "touch me":
                 result = check_action(action)
                 if result == "pass":
-                    display.show(Image.YES)
                     success = "1"
                     running = False
                 elif result == "fail":
-                    display.show(Image.NO)
                     success = "0"
                     running = False
     
             elif action.lower() == "face up":
                 result = check_action(action)
                 if result == "pass":
-                    display.show(Image.YES)
                     success = "1"
                     running = False
                 elif result == "fail":
-                    display.show(Image.NO)
                     success = "0"
                     running = False
     
             elif action.lower() == "face down":
                 result = check_action(action)
                 if result == "pass":
-                    display.show(Image.YES)
                     success = "1"
                     running = False
                 elif result == "fail":
-                    display.show(Image.NO)
                     success = "0"
                     running = False
     
             elif action.lower() == "left":
                 result = check_action(action)
                 if result == "pass":
-                    display.show(Image.YES)
                     success = "1"
                     running = False
                 elif result == "fail":
-                    display.show(Image.NO)
                     success = "0"
                     running = False
     
             elif action.lower() == "right":
                 result = check_action(action)
                 if result == "pass":
-                    display.show(Image.YES)
                     success = "1"
                     running = False
                 elif result == "fail":
-                    display.show(Image.NO)
                     success = "0"
                     running = False
     
             elif action.lower() == "button a":
                 result = check_action(action)
                 if result == "pass":
-                    display.show(Image.YES)
                     success = "1"
                     running = False
                 elif result == "fail":
-                    display.show(Image.NO)
                     success = "0"
                     running = False
     
             elif action.lower() == "button b":
                 result = check_action(action)
                 if result == "pass":
-                    display.show(Image.YES)
                     success = "1"
                     running = False
                 elif result == "fail":
-                    display.show(Image.NO)
                     success = "0"
                     running = False
             sleep(100)
@@ -182,9 +159,15 @@ def game(round, action, simon):
     if counter > 0 and simon == "0":        ####
         success = "0"
     radio.send(userID+success)
-    if counter == 0:
+    if success == "0":
         display.show(Image.NO)
-    
+        music.play(["D3:3", "Db2:3"])
+    elif success == "1":
+        display.show(Image.YES)
+        music.play(music.BA_DING)
+    sleep(50)
 
+        
+        
 
 
